13f57bc73caf1033630a8ffd6a787453ce09ac8e
Matheus Renato <teus.rbe06@gmail.com>
2025-10-15 18:14:48 -0300

    ajuste

--- atualizar.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "=== Atualizando repositório local com o remoto ===" -ForegroundColor Cyan
-
-git fetch origin
-git checkout main
-git pull origin main --rebase
-
-Write-Host "Repositorio sincronizado" -ForegroundColor Green
